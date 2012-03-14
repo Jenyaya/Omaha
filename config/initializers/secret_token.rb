d5a1a3572ea01369c59302caa997165a828125a2
Jenyaya <jenyaya@online.ua>
2012-03-14 06:58:42 +0000

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omaha::Application.config.secret_token = 'e223ba3b5b258defee053a1345388d7a3701dba4d70bfbcbc79327d93dea23e2edcc62ebba97e3c48aa0905b0ecae1aa8aa8ef452cc8fbf03617b9894c2ab3fc'
